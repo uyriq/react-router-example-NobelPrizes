4b1a76590153eecca96e8f570bdec7c45c2dc79a
uyriq <105006434+uyriq@users.noreply.github.com>
2022-10-24 22:54:25 +0300

    done task2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { HomePage } from "./pages";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HomePage, ListPage } from "./pages";
 export default function App() {
   return (
     <Router>
-      <Route path="/" exact={true}>
-        <HomePage />
-      </Route>
+      <Switch>
+        <Route path="/list" exact={true}>
+          <ListPage />
+        </Route>
+        <Route path="/" exact={true}>
+          <HomePage />
+        </Route>
+      </Switch>
     </Router>
   );
 }
