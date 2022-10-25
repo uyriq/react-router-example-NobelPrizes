a927defd35061f9d3d8d52b7a90e3c235788cf2d
uyriq <105006434+uyriq@users.noreply.github.com>
2022-10-25 21:04:05 +0300

    link

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-
-import styles from './home.module.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./home.module.css";
 
 export function HomePage() {
   return (
@@ -9,7 +9,11 @@ export function HomePage() {
         <form className={styles.form}>
           <h1 className={styles.heading}>Nobel Prize Library</h1>
         </form>
+
         <p>1901-2020</p>
+        <Link className={styles.link} to="/list">
+          View catalog
+        </Link>
       </div>
     </div>
   );
